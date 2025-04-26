6e79a9165ac5d566e32560502c074bc4f60d1e5b
sri5099 <sri11321@gmail.com>
2025-04-26 17:31:36 +0530

    final

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,9 @@ import { Card } from "@/components/ui/card";
 export const Hero = () => {
   return (
     <><div className="flex items-center justify-between mb-4">
-      <img src="public/assets/image 54.png" alt="Office Illustration" className="w-auto h-auto" />
-      <div className="flex items-center border-2 border-[#FFB800] rounded-full px-4 py-2">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#FFB800] mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+      <img src="public/assets/image 54.png" alt="Office Illustration" className="w-auto h-auto ml-4 mt-4" />
+      <div className="flex items-center border-2 border-[#FFB800] rounded-full px-4 py-2 mr-6">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#FFB800] " viewBox="0 0 24 24" fill="none" stroke="#F2B304" strokeWidth="2">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
         </svg>
       </div>
